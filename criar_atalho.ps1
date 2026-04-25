f0e8a55a2db111a2eabbabd45057c274cf833948
Gabriel C <gabrielndcarvalho@gmail.com>
2026-04-25 19:14:22 -0300

    fix: criar_atalho.ps1 com caminho relativo

--- criar_atalho.ps1
+++ criar_atalho.ps1
@@ -1,8 +1,9 @@
+$root = Split-Path -Parent $MyInvocation.MyCommand.Path
 $ws = New-Object -ComObject WScript.Shell
 $desktop = [System.Environment]::GetFolderPath('Desktop')
 $atalho = $ws.CreateShortcut("$desktop\Gerenciador de Cursos.lnk")
-$atalho.TargetPath = "C:\gerenciador-cursos\iniciar_silencioso.vbs"
-$atalho.Arguments = '"C:\gerenciador-cursos\iniciar.bat"'
-$atalho.WorkingDirectory = "C:\gerenciador-cursos"
-$atalho.IconLocation = "C:\gerenciador-cursos\web\assets\logo_icon.ico"
+$atalho.TargetPath = "$root\iniciar_silencioso.vbs"
+$atalho.Arguments = "`"$root\iniciar.bat`""
+$atalho.WorkingDirectory = $root
+$atalho.IconLocation = "$root\web\assets\logo_icon.ico"
 $atalho.Save()
